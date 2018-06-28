055f3a141a80eae7ce29fa09bb06465a7f69fcd3
Devaev Maxim <mdevaev@gmail.com>
2018-06-28 03:54:28 +0300

    wscli: changed default url

--- kvmd/wscli.py
+++ kvmd/wscli.py
@@ -40,7 +40,7 @@ async def _run_client(loop: asyncio.AbstractEventLoop, url: str) -> None:
 
 def main() -> None:
     parser = argparse.ArgumentParser()
-    parser.add_argument("-u", "--url", default="http://localhost:8080")
+    parser.add_argument("-u", "--url", default="http://localhost:8081/ws")
     options = parser.parse_args()
 
     loop = asyncio.get_event_loop()
