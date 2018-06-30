c0aee642467655952d701e37f284a7a589d638e2
Devaev Maxim <mdevaev@gmail.com>
2018-06-30 05:55:02 +0300

    reorganized project structure

--- os/platforms/v1/run.sh
+++ os/platforms/v1/run.sh
@@ -5,13 +5,14 @@ set -x
 . ../functions.sh
 
 
-cat config.txt > "$FS/boot/config.txt"
+cp config.txt "$FS/boot/config.txt"
 pkg_install \
 	kvmd \
 	mjpg-streamer-pikvm \
 	nginx
 
 cp index.html "$FS/srv/http/"
+cp kvmd.yaml "$FS/etc/"
 cp nginx.conf "$FS/etc/nginx/"
 rpi systemctl enable kvmd
 rpi systemctl enable nginx
