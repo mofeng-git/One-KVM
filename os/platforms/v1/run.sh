95a5bbb3676e1d908959f064158a1355bbd5b89e
Devaev Maxim <mdevaev@gmail.com>
2018-07-03 06:59:40 +0300

    fixed os freezes

--- os/platforms/v1/run.sh
+++ os/platforms/v1/run.sh
@@ -5,14 +5,16 @@ set -x
 . ../functions.sh
 
 
-cp config.txt "$FS/boot/config.txt"
 pkg_install \
 	kvmd \
 	mjpg-streamer-pikvm \
 	nginx
 
+cp config.txt "$FS/boot/"
+cp 99-pikvm.conf "$FS/etc/sysctl.d/"
 cp index.html "$FS/srv/http/"
 cp kvmd.yaml "$FS/etc/"
 cp nginx.conf "$FS/etc/nginx/"
+
 rpi systemctl enable kvmd
 rpi systemctl enable nginx
