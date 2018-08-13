f2dde902ceebba4d9c9865295b73a83a30f1638c
Devaev Maxim <mdevaev@gmail.com>
2018-08-13 10:29:54 +0300

    Bump version: 0.52 → 0.53

--- kvmd/PKGBUILD
+++ kvmd/PKGBUILD
@@ -3,7 +3,7 @@
 
 
 pkgname="kvmd"
-pkgver="0.52"
+pkgver="0.53"
 pkgrel="1"
 pkgdesc="The main Pi-KVM daemon"
 arch=("any")
--- kvmd/kvmd/server.py
+++ kvmd/kvmd/server.py
@@ -29,7 +29,7 @@ from .logging import get_logger
 
 
 # =====
-__version__ = "0.52"
+__version__ = "0.53"
 
 
 @functools.lru_cache()
