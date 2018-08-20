a4b6f4fb98200829eba4c93597b7622196a2265c
Devaev Maxim <mdevaev@gmail.com>
2018-08-20 07:43:23 +0300

    no ie allowed

--- kvmd/web/js/main.js
+++ kvmd/web/js/main.js
@@ -1,6 +1,15 @@
 function main() {
-	var hid = new Hid();
-	var ui = new Ui(hid);
-	new Session(new Atx(), hid, new Msd());
-	new Stream(ui);
+	if (
+		!window.navigator
+		|| window.navigator.userAgent.indexOf("MSIE ") > 0
+		|| window.navigator.userAgent.indexOf("Trident/") > 0
+		|| window.navigator.userAgent.indexOf("Edge/") > 0
+	) {
+		$("bad-browser-modal").style.visibility = "visible";
+	} else {
+		var hid = new Hid();
+		var ui = new Ui(hid);
+		new Session(new Atx(), hid, new Msd());
+		new Stream(ui);
+	}
 }
