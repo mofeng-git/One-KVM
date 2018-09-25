089bac2bcd1e49ba77aacd6853ef93e03a051b96
Devaev Maxim <mdevaev@gmail.com>
2018-09-26 02:20:58 +0300

    moved hid to kvmd

--- kvmd/genmap.py
+++ kvmd/genmap.py
@@ -18,13 +18,13 @@ def main() -> None:
                 parts = row.split()
                 keymap.append((int(parts[0]), parts[1], parts[2]))
 
-    with open("../kvmd/kvmd/data/keymap.yaml", "w") as kvmd_yaml_file:
+    with open("kvmd/data/keymap.yaml", "w") as kvmd_yaml_file:
         yaml.dump({
             js_key: code
             for (code, _, js_key) in sorted(keymap, key=operator.itemgetter(2))
         }, kvmd_yaml_file, indent=4, default_flow_style=False)
 
-    with open("src/keymap.h", "w") as hid_header_file:
+    with open("hid/src/keymap.h", "w") as hid_header_file:
         hid_header_file.write("#pragma once\n\n#include <HID-Project.h>\n\n#include \"inline.h\"\n\n\n")
         hid_header_file.write("INLINE KeyboardKeycode keymap(uint8_t code) {\n\tswitch(code) {\n")
         for (code, hid_key, _) in sorted(keymap, key=operator.itemgetter(1)):
