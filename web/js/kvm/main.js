4a988136d1a3d27befab46819d1f41331a6eb49a
Devaev Maxim <mdevaev@gmail.com>
2018-11-21 07:56:27 +0300

    refactoring

--- web/js/kvm/main.js
+++ web/js/kvm/main.js
@@ -7,7 +7,7 @@ function main() {
 		tools.setOnClick($("show-about-button"), () => ui.showWindow($("about-window")));
 		tools.setOnClick($("show-keyboard-button"), () => ui.showWindow($("keyboard-window")));
 		tools.setOnClick($("show-stream-button"), () => ui.showWindow($("stream-window")));
-		tools.setOnClick($("open-log-button"), () => window.open("kvmd/log?seek=3600&follow=1", "_blank"));
+		tools.setOnClick($("open-log-button"), () => window.open("/kvmd/log?seek=3600&follow=1", "_blank"));
 
 		ui.showWindow($("stream-window"));
 
