97d8d6ea4c2ce88e6045f89461ac0ca8caff0830
Devaev Maxim <mdevaev@gmail.com>
2018-11-23 09:35:01 +0300

    index

---
+++ web/js/index/main.js
@@ -0,0 +1,58 @@
+function main() {
+	__setAppText();
+	__loadKvmdInfo();
+}
+
+function __setAppText() {
+	var url = window.location.href;
+	$("app-text").innerHTML = `
+		<span class="code-comment"># On Linux using Chromium/Chrome via any terminal:<br>
+		$</span> &bsol;which chromium 2>/dev/null || which chrome 2>/dev/null&bsol; --app="${url}"<br>
+		<br>
+		<span class="code-comment"># On MacOS using Terminal application:<br>
+		$</span> /Applications/Google&bsol; Chrome.app/Contents/MacOS/Google&bsol; Chrome --app="${url}"<br>
+		<br>
+		<span class="code-comment"># On Windows via cmd.exe:<br>
+		C:&bsol;&gt;</span> start chrome --app="${url}"
+	`;
+}
+
+function __loadKvmdInfo() {
+	var http = tools.makeRequest("GET", "/kvmd/info", function() {
+		if (http.readyState === 4) {
+			if (http.status === 200) {
+				var info = JSON.parse(http.responseText).result;
+
+				var apps = Object.values(info.extras).sort(function(a, b) {
+					if (a["place"] < b["place"]) {
+						return -1;
+					} else if (a["place"] > b["place"]) {
+						return 1;
+					} else {
+						return 0;
+					}
+				});
+				apps.forEach(function(app) {
+					$("apps").innerHTML += `
+						<li>
+							<div class="app">
+								<a href="${app.path}">
+									<div>
+										<img class="svg-gray" src="${app.icon}">
+										${app.name}
+									</div>
+								</a>
+							</div>
+						</li>
+					`;
+				});
+
+				if (info.meta && info.meta.server && info.meta.server.host) {
+					$("kvmd-meta-server-host").innerHTML = info.meta.server.host;
+				}
+			} else {
+				setTimeout(__loadKvmdInfo, 1000);
+			}
+		}
+	});
+}
