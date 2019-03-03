9ecb7abbcbd0c311fb4e959f4fb5db97cb9ab88a
Devaev Maxim <mdevaev@gmail.com>
2019-03-03 12:42:04 +0300

    Bump version: 0.141 → 0.142

--- PKGBUILD
+++ PKGBUILD
@@ -13,7 +13,7 @@ for _platform in $_PLATFORMS; do
 	done
 done
 pkgbase=kvmd
-pkgver=0.141
+pkgver=0.142
 pkgrel=1
 pkgdesc="The main Pi-KVM daemon"
 url="https://github.com/pi-kvm/kvmd"
--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ from setuptools import setup
 def main() -> None:
     setup(
         name="kvmd",
-        version="0.141",
+        version="0.142",
         url="https://github.com/pi-kvm/pi-kvm",
         license="GPLv3",
         author="Maxim Devaev",
