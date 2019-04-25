a89612e5ac6ae03089500768bed9ef049ce75c4b
Devaev Maxim <mdevaev@gmail.com>
2019-04-25 04:15:59 +0300

    Bump version: 0.155 → 0.156

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ from setuptools import setup
 def main() -> None:
     setup(
         name="kvmd",
-        version="0.155",
+        version="0.156",
         url="https://github.com/pi-kvm/kvmd",
         license="GPLv3",
         author="Maxim Devaev",
