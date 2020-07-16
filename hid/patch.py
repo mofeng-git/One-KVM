73069ec44eb0037f8d0d75213b6d59afa9a4c81f
Devaev Maxim <mdevaev@gmail.com>
2020-07-16 22:32:35 +0300

    fixed patching

--- hid/patch.py
+++ hid/patch.py
@@ -11,7 +11,7 @@ env_path = join(deps_path, env["PIOENV"])
 flag_path = join(env_path, ".patched")
 
 if not exists(flag_path):
-    env.Execute(f"patch -p1 -d {join(env_path, 'HID-Project_ID523')} < {join('patches', 'absmouse.patch')}")
+    env.Execute(f"patch -p1 -d {join(env_path, 'HID-Project')} < {join('patches', 'absmouse.patch')}")
 
     def touch_flag(*_, **__) -> None:
         with open(flag_path, "w") as flag_file:
