5464bc2297e69b493b37c6dffd7fec66d55d6ad9
Devaev Maxim <mdevaev@gmail.com>
2020-09-13 19:18:12 +0300

    fixed AioPinsReader's main loop

--- kvmd/aiogp.py
+++ kvmd/aiogp.py
@@ -93,16 +93,15 @@ class AioPinsReader(threading.Thread):
             self.__loop.call_soon_threadsafe(self.__notifier.notify_sync)
 
             while not self.__stop_event.is_set():
-                ev_lines = lines.event_wait(10)
+                ev_lines = lines.event_wait(1)
                 if ev_lines:
-                    for ev_lines in lines.event_wait(1):
-                        for ev_line in ev_lines:
-                            event = ev_line.event_read()
-                            if event.type == gpiod.LineEvent.RISING_EDGE:
-                                value = True
-                            elif event.type == gpiod.LineEvent.FALLING_EDGE:
-                                value = False
-                            else:
-                                raise RuntimeError(f"Invalid event {event} type: {event.type}")
-                            self.__state[event.source.offset()] = value
-                        self.__loop.call_soon_threadsafe(self.__notifier.notify_sync)
+                    for ev_line in ev_lines:
+                        event = ev_line.event_read()
+                        if event.type == gpiod.LineEvent.RISING_EDGE:
+                            value = True
+                        elif event.type == gpiod.LineEvent.FALLING_EDGE:
+                            value = False
+                        else:
+                            raise RuntimeError(f"Invalid event {event} type: {event.type}")
+                        self.__state[event.source.offset()] = value
+                    self.__loop.call_soon_threadsafe(self.__notifier.notify_sync)
