cacb12791b1d914111f566dcc2f992b34e744d10
Devaev Maxim <mdevaev@gmail.com>
2021-04-16 23:46:58 +0300

    Bump version: 2.51 → 2.52

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ def main() -> None:
 
     setup(
         name="kvmd",
-        version="2.51",
+        version="2.52",
         url="https://github.com/pikvm/kvmd",
         license="GPLv3",
         author="Maxim Devaev",
