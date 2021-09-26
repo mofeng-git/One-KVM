c585dfc483fb5af144bf3bd27732545ac6cba107
Maxim Devaev <mdevaev@gmail.com>
2021-09-26 08:48:54 +0300

    parse yes/no/on/off as string

--- testenv/tests/test_yamlconf.py
+++ testenv/tests/test_yamlconf.py
@@ -20,55 +20,23 @@
 # ========================================================================== #
 
 
-import os
+import pathlib
+import textwrap
 
-from typing import IO
-from typing import Any
-
-import yaml
-import yaml.nodes
-import yaml.resolver
-import yaml.constructor
-
-from .. import tools
+from kvmd.yamlconf.loader import load_yaml_file
 
 
 # =====
-def load_yaml_file(path: str) -> Any:
-    with open(path) as yaml_file:
-        try:
-            return yaml.load(yaml_file, _YamlLoader)
-        except Exception as err:
-            # Reraise internal exception as standard ValueError and show the incorrect file
-            raise ValueError(f"Invalid YAML in the file {path!r}:\n{tools.efmt(err)}") from None
-
-
-# =====
-class _YamlLoader(yaml.SafeLoader):
-    def __init__(self, yaml_file: IO) -> None:
-        super().__init__(yaml_file)
-        self.__root = os.path.dirname(yaml_file.name)
-
-    def include(self, node: yaml.nodes.ScalarNode) -> Any:
-        path = os.path.join(self.__root, str(self.construct_scalar(node)))
-        return load_yaml_file(path)
-
-
-_YamlLoader.add_constructor("!include", _YamlLoader.include)
-
-
-# =====
-def _disable_some_bools() -> None:
-    # https://stackoverflow.com/questions/36463531
-    resolvers = yaml.resolver.Resolver.yaml_implicit_resolvers
-    for key in "oOyYnN":
-        resolvers[key] = [
-            resolver
-            for resolver in resolvers[key]
-            if resolver[0] != "tag:yaml.org,2002:bool"
-        ]
-        if len(resolvers) == 0:
-            del resolvers[key]
-
-
-_disable_some_bools()
+def test_load_yaml_file__bools(tmp_path: pathlib.Path) -> None:  # type: ignore
+    pobj = tmp_path / "test.yaml"
+    pobj.write_text(textwrap.dedent("""
+        a: true
+        b: false
+        c: yes
+        d: no
+    """))
+    data = load_yaml_file(str(pobj))
+    assert data["a"] is True
+    assert data["b"] is False
+    assert data["c"] == "yes"
+    assert data["d"] == "no"
