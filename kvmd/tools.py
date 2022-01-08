93a57e5e37271905a9678a94827d73d24be9e18b
Maxim Devaev <mdevaev@gmail.com>
2022-01-08 07:18:43 +0300

    fixed remap() type to int

--- kvmd/tools.py
+++ kvmd/tools.py
@@ -34,7 +34,7 @@ from typing import TypeVar
 
 # =====
 def remap(value: int, in_min: int, in_max: int, out_min: int, out_max: int) -> int:
-    return (value - in_min) * (out_max - out_min) // (in_max - in_min) + out_min
+    return int((value - in_min) * (out_max - out_min) // (in_max - in_min) + out_min)
 
 
 # =====
