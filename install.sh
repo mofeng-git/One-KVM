40ad9aa0b66baff11ce0bf76ba3078765ede06ed
mofeng <3285338098@qq.com>
2023-11-25 23:20:54 +0800

    添加注释

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ if [[ "$PYVER" != *"3.10"* && $(which python3.10) != *"python"* ]]; then
 fi
 
 cp ./patch/meson8b-onecloud.dtb /boot/dtb/meson8b-onecloud.dtb && echo "设备树文件覆盖成功！"
+#此为危险操作，同时会覆盖MBR分区，请在没有自行分区前执行，否则会丢失分区数据导致挂载了EMMC其他分区的系统无法启动！
 gzip -dc ./patch/Boot_SkipUSBBurning.gz | dd of=/dev/mmcblk1 && echo "覆盖引导成功！"
 
 bash <(curl -sSL https://gitee.com/SuperManito/LinuxMirrors/raw/main/ChangeMirrors.sh) --source mirrors.tuna.tsinghua.edu.cn --updata-software false --web-protocol http && echo "换源成功！"
