fc9fff07dbd27ee9cb47cbbc075b89a353a4d076
mofeng <3285338098@qq.com>
2023-11-26 00:53:28 +0800

    更新

--- install.sh
+++ install.sh
@@ -31,6 +31,9 @@ fi
 cp ./patch/meson8b-onecloud.dtb /boot/dtb/meson8b-onecloud.dtb && echo "设备树文件覆盖成功！"
 #此为危险操作，会覆盖MBR分区，请在没有自行分区前执行，否则会丢失分区数据导致挂载了EMMC其他分区的系统无法启动！
 if [ -f "./installed.txt" ]; then
+#防止新证书覆盖失败
+    rm /etc/kvmd/nginx/ssl/server.crt
+    rm /etc/kvmd/nginx/ssl/server.key
     echo "跳过覆盖引导！"
 else
     gzip -dc ./patch/Boot_SkipUSBBurning.gz | dd of=/dev/mmcblk1 && echo "One-KVM V0.4" >> installed.txt && echo "覆盖引导成功！"
