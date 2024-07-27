d7675cdf231896851c492e1e0e719d2ccdc28082
Maxim Devaev <mdevaev@gmail.com>
2024-07-27 15:29:30 +0300

    Bump version: 4.2 → 4.3

--- PKGBUILD
+++ PKGBUILD
@@ -39,7 +39,7 @@ for _variant in "${_variants[@]}"; do
 	pkgname+=(kvmd-platform-$_platform-$_board)
 done
 pkgbase=kvmd
-pkgver=4.2
+pkgver=4.3
 pkgrel=1
 pkgdesc="The main PiKVM daemon"
 url="https://github.com/pikvm/kvmd"
--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ def main() -> None:
 
     setup(
         name="kvmd",
-        version="4.2",
+        version="4.3",
         url="https://github.com/pikvm/kvmd",
         license="GPLv3",
         author="Maxim Devaev",
