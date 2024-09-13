dffebf003e47b2b9b43ac28665300db1b5e5d102
mofeng-git <mofeng654321@hotmail.com>
2024-09-13 23:25:19 +0800

    添加了如下 Docker 环境变量： USERNAME 用户账号 PASSWORD 用户密码 NOSSL 禁用 HTTPS，启用 HTTP

--- configs/kvmd/init.sh
+++ configs/kvmd/init.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
-echo  "One-KVM pre-starting..."
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+NC='\033[0m'
 
-if [ "$OTG" == "1" ]; then
-    echo "OTG is enabled."
-    
-    if [ ! -f /etc/kvmd/.otg_flag ]; then
-        echo "Enable One-KVM otg config."
-        touch /etc/kvmd/.otg_flag
+echo -e "${GREEN}One-KVM pre-starting...${NC}"
+
+#仅首次运行，用于初始化配置文件
+if [ ! -f /etc/kvmd/.init_flag ]; then
+    #生成 ssl 证书 和 vnc 证书
+    /usr/share/kvmd/kvmd-gencert --do-the-thing
+    /usr/share/kvmd/kvmd-gencert --do-the-thing --vnc
+    #生成 nginx 配置文件
+    if [ "$NOSSL" = 1 ]; then
+        python -m kvmd.apps.ngxmkconf /etc/kvmd/nginx/nginx.conf.mako /etc/kvmd/nginx/nginx.conf  -o nginx/https/enabled=false
+    else
+        python -m kvmd.apps.ngxmkconf /etc/kvmd/nginx/nginx.conf.mako /etc/kvmd/nginx/nginx.conf
+    fi
+    #OTG 初始化修改默认配置文件
+    if [ "$OTG" == "1" ]; then
+        echo -e "${GREEN}One-KVM OTG is enabled.${NC}"
         sed -i "s/ch9329/otg/g" /etc/kvmd/override.yaml
 	    sed -i "s/device: \/dev\/ttyUSB0//g" /etc/kvmd/override.yaml
         cat >> /etc/kvmd/supervisord.conf << EOF
@@ -24,15 +38,34 @@ stdout_logfile_maxbytes = 0
 redirect_stderr=true
 EOF
     fi
+    #/dev/video0 设备优先级高于 /dev/kvmd-video
     if [ -f /dev/video0 ]; then
+        echo -e "${GREEN}Found /dev/video0, use it as kvmd video device.${NC}"
         sed -i "s/\/dev\/kvmd-video/\/dev\/video0/g" /etc/kvmd/override.yaml
     fi
+    #设置用户账号密码
+    if [ ! -z "$USERNAME" ] && [ ! -z "$PASSWORD" ]; then
+        python -m kvmd.apps.htpasswd del admin
+        echo $PASSWORD | python -m kvmd.apps.htpasswd set -i  "$USERNAME"
+        echo "$PASSWORD -> $USERNAME:$PASSWORD" > /etc/kvmd/vncpasswd
+        echo "$USERNAME:$PASSWORD -> $USERNAME:$PASSWORD" > /etc/kvmd/ipmipasswd
+    else
+        echo -e "${YELLOW} USERNAME and PASSWORD environment variables is not set, using defalut(admin/admin).${NC}"
+    fi
+    #新建 flag 标记文件
+    touch /etc/kvmd/.init_flag
+fi
+
+#尝试挂载 usb_gadget
+if [ "$OTG" == "1" ]; then
+    echo "Trying OTG Port..."
     if [ -d /sys/kernel/config/usb_gadget/kvmd ]; then
-        echo -e "\033[31m Usb_gadget kvmd exists, please reboot your host system. \033[0m"
+        echo -e "${RED}Usb_gadget kvmd exists, please reboot your host system. ${NC}"
         exit -1
     elif [ ! -d /sys/kernel/config/usb_gadget ]; then
         mount -t configfs none /sys/kernel/config
     fi
-    
 fi
-supervisord -c /etc/kvmd/supervisord.conf
+
+echo -e "${GREEN}One-KVM starting...${NC}"
+exec supervisord -c /etc/kvmd/supervisord.conf
