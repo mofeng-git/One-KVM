be3e97178d6ed1226216246253bf54c86bc5ebf9
Maxim Devaev <mdevaev@gmail.com>
2025-01-21 05:41:25 +0200

    moved to python-3.13

--- PKGBUILD
+++ PKGBUILD
@@ -46,8 +46,8 @@ url="https://github.com/pikvm/kvmd"
 license=(GPL)
 arch=(any)
 depends=(
-	"python>=3.12"
-	"python<3.13"
+	"python>=3.13"
+	"python<3.14"
 	python-yaml
 	python-aiohttp
 	python-aiofiles
--- setup.py
+++ setup.py
@@ -143,7 +143,7 @@ def main() -> None:
         classifiers=[
             "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
             "Development Status :: 5 - Production/Stable",
-            "Programming Language :: Python :: 3.12",
+            "Programming Language :: Python :: 3.13",
             "Topic :: System :: Systems Administration",
             "Operating System :: POSIX :: Linux",
             "Intended Audience :: System Administrators",
