3014edffbb27ad4fae90972f593096d739ccf280
mofeng-git <mofeng654321@hotmail.com>
2026-09-05 22:40:01 +0800

    fix(db): 使用 FULL 同步确保事务提交时落盘

--- src/db/pool.rs
+++ src/db/pool.rs
@@ -23,7 +23,7 @@ impl DatabasePool {
             .create_if_missing(true)
             .foreign_keys(true)
             .journal_mode(SqliteJournalMode::Wal)
-            .synchronous(SqliteSynchronous::Normal)
+            .synchronous(SqliteSynchronous::Full)
             .busy_timeout(Duration::from_secs(5));
 
         let pool = SqlitePoolOptions::new()
